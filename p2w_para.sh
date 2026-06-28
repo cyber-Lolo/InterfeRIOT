73251dbb0695f370a3f81c09ca13ba106990dd32
Lolo <92681850+cyber-Lolo@users.noreply.github.com>
2026-06-28 18:56:46 +0100

    Delete p2w_para.sh

--- p2w_para.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# usage: bash run_parallel.sh <name> <max_jobs>
-NAME=$1
-START=$2
-END=$3
-MAX_JOBS=${4:-18}
-
-seq "$START" "$END" \
-  | xargs -P "$MAX_JOBS" -I {} python3 p2w.py "$NAME" {}
